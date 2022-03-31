d28c0b2119a955f99d330175da1864dac8e81443
Carlos Lopez <clopez@applaudostudios.com>
2022-03-30 22:58:53 -0600

    add work for JavaScript part 2

--- scripts/main.js
+++ scripts/main.js
@@ -126,3 +126,142 @@ function objectFunction(name, age) {
 
 const funcObject = objectFunction('Second Custom Object', 20);
 console.log(funcObject);
+
+/* Ejemplo de encapsulacion */
+
+// Convencion
+const encapObject = {
+  _id: 1
+}
+
+// Functional Scope
+function objectFact() {
+  let _name = 'Test';
+  let _age = 10;
+  return {
+    height: '160cm',
+    name: function() {
+      return _name;
+    },
+    age: function() {
+      return _age;
+    }
+  }
+}
+
+const funcObjectEncap = objectFact();
+console.log(funcObjectEncap.age());
+
+// Clousure 
+
+function clousure1() {
+  const _height = '160cm';
+  const _rank = 'Sr.';
+  const privateFunc = function() {
+    console.log('Hola!');
+  }
+  return function(id){
+    if (id < 1) {
+      console.log('No hay nadie registrado');
+    } else {
+      return {
+        name: 'Sr. Nicolas',
+        age: 56,
+        info: function() {
+          privateFunc();
+          console.log('Mi rango es ' + _rank, 'Mi altura es '+ _height);
+        }
+      }
+    }
+  }
+};
+
+const human = clousure1(null, 0);
+console.log(human(4).info());
+
+// Metodos
+const objectWithMethods = {
+  name: 'Rick',
+  age: 10,
+  greet: function(name) {
+    return 'Hola ' + name +', mi nombre es '+ this.name;
+  },
+  aboutMe: function() {
+    return () => {
+      return 'Hola señor, mi nombre es ' + this.name;
+    }
+  }
+}
+
+console.log(objectWithMethods.aboutMe()());
+
+// Prototipos
+const catPrototype = {
+  name: 'Tom',
+  age: 10,
+  miau() {
+    console.log(this.name);
+  }
+};
+const newCat = Object.create(catPrototype);
+newCat.name = 'Lucas';
+console.log(newCat.miau());
+
+//Polimorfismo
+const shape = {
+  height: 0,
+  width: 0,
+  area(){}
+}
+
+const triangle = Object.create(shape);
+triangle.height = 10;
+triangle.width = 4;
+triangle.area = function() {
+  return (this.height * this.width) / 2;
+}
+
+const rectangle = Object.create(shape);
+rectangle.height = 10;
+rectangle.width = 4;
+rectangle.area = function() {
+  return this.height * this.width;
+}
+rectangle.name = 'Rectangulo nuevo';
+
+console.log(triangle.area(), rectangle.area(), rectangle.name);
+
+// Getters, Setters y Static
+
+class Tree {
+  _height;
+  _age;
+  _multiplier;
+  constructor(height, age) {
+    this._height = height;
+    this._age = age;
+    this._multiplier = 0.5;
+  }
+  get age() {
+    return this._age;
+  }
+  set age(newAge) {
+    this._age = newAge;
+  }
+
+  get currentHeight() {
+    return this._height * this._age * this._multiplier;
+  }
+
+  static doc() {
+    return 'Esta es la clase prototipo para un arbol básico';
+  }
+}
+
+console.log(Tree.doc());
+
+const appleTree = new Tree(5, 3);
+console.log(appleTree.age, appleTree.currentHeight);
+
+appleTree.age = 10;
+console.log(appleTree.currentHeight);
